a3df8dbfeb005315e94bc6dbb270272cbd60e43c
Swikkft <pibum@naver.com>
2024-10-03 17:14:42 +0900

    Add: Proj init

---
+++ Independent-Capstone-Design/App/Independent_Capstone_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Independent_Capstone_DesignApp.swift
+//  Independent-Capstone-Design
+//
+//  Created by 박익범 on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Independent_Capstone_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Independent-Capstone-Design/Present/Main/View/MainView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
